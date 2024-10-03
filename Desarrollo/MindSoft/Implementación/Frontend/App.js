843a3603481447e16f5c1655a5ade24887e52703
GDaniel <gerardo.ladera@unmsm.edu.pe>
2024-10-03 18:22:51 -0500

    Corrección de carpeta de proyecto frontend

--- Desarrollo/MindSoft/Implementación/Frontend/App.js
+++ Desarrollo/MindSoft/Implementación/Frontend/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Prueba app!</Text>
+      <Text>Prueba apppp!</Text>
       <StatusBar style="auto" />
     </View>
   );
