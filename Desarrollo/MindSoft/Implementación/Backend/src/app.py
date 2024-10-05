738e36b0d92cb1bb6048497ba47bed898388f7a8
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-10-04 22:21:49 -0500

    add: envexample

--- Desarrollo/MindSoft/Implementación/Backend/src/app.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/app.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from typing import Annotated
+
+from .services.database import create_tables
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+  await create_tables()
+
 @app.get("/helloworld")
-def hello_world():
+async def hello_world():
+  db = get_db()
   return { "message": "Hello World" }
