9db1b9b211f7b6e529f8ce9ecf214c3c8972fb9e
DAVIDcc <cisneros02culaca@gmail.com>
2024-10-06 13:46:49 -0500

    Corrección de carpeta de proyecto frontend

--- Desarrollo/MindSoft/Implementación/Frontend/App.js
+++ Desarrollo/MindSoft/Implementación/Frontend/App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import login from './src/screens/login';
+import Confirmacion from './src/screens/Confirmacion';
 
 export default function App() {
+  
+  const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Prueba apppp!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="login" component={login} 
+      options={{
+        title: "",
+        headerTinteColor: "white",
+        headerTitleAling: "center",
+        headerStyle: { backgroundColor: "#0B72D2"},
+      }}
+      />
+      <Stack.Screen name="Confirmacion" component={Confirmacion}
+      options={{
+        headerShown: false, // Oculta el header solo en la pantalla de Home
+      }} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+   <NavigationContainer>
+    <MyStack/>
+   </NavigationContainer>
+  );
+}
