47b195985a2461713e1985b2fd688c6f1dec99db
BrayanAlq <brayan.alquizar@unmsm.edu.pe>
2024-10-06 09:55:24 -0500

    fix: controller daily_rating get

--- Desarrollo/MindSoft/Implementación/Backend/src/controllers/daily_rating.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/controllers/daily_rating.py
@@ -10,12 +10,14 @@ from sqlalchemy.orm import Session
 from ..utils.jwt_util import get_current_user
 
 def get_daily_rating(db: Session , user: UserToJwt):
-    daily_rating = db.query(DailyRating).filter(DailyRating.user_id == user.user_id).first()
+    daily_rating = db.query(DailyRating).filter(DailyRating.user_id == user.user_id).all()
     if not daily_rating:
         raise HTTPException(status_code=404, detail="Daily rating not found")
     return daily_rating
 
 def create_daily_rating(daily_rating: DailyRatingCreate, db: Session,user:UserToJwt):
+    if daily_rating.rating>5 or daily_rating.rating<1:
+        raise HTTPException(status_code=400, detail="Rating must be between 1 and 5")
     db_daily_rating = DailyRating(
         user_id=user.user_id,
         rating=daily_rating.rating,
