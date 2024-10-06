9b2e24780931102394f905093938fcbc4782c425
BrayanAlq <brayan.alquizar@unmsm.edu.pe>
2024-10-05 19:18:36 -0500

    add: daily_rating in app

--- Desarrollo/MindSoft/Implementación/Backend/src/app.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/app.py
@@ -6,10 +6,12 @@ from .services.database import create_tables
 from .utils.jwt_util import get_current_user
 from .models import User
 from .routes.login import login_router
+from .routes.daily_rating import daily_rating_router
 
 app = FastAPI()
 
 app.include_router(login_router)
+app.include_router(daily_rating_router)
 
 @app.on_event("startup")
 def startup():
