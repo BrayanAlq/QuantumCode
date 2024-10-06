24f7df614c59111409f53b5f954389860f8109a1
BrayanAlq <brayan.alquizar@unmsm.edu.pe>
2024-10-05 19:42:51 -0500

    fix: route daily_rating

--- Desarrollo/MindSoft/Implementación/Backend/src/routes/daily_rating.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/routes/daily_rating.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from ..schemas.daily_rating import DailyRating
+from ..schemas.daily_rating import DailyRatingCreate
 from ..services.database import get_db
 from ..utils.jwt_util import get_current_user
+from ..schemas.user import UserToJwt
 from ..controllers.daily_rating import get_daily_rating, create_daily_rating
 
 daily_rating_router = APIRouter()
@@ -14,6 +15,6 @@ def get_user_daily_rating(db: Session = Depends(get_db), user: UserToJwt = Depen
     return response
 
 @daily_rating_router.post("/user-daily_rating")
-def create_user_daily_rating(daily_rating: DailyRating, db: Session = Depends(get_db), user: UserToJwt = Depends(get_current_user)):
+def create_user_daily_rating(daily_rating: DailyRatingCreate, db: Session = Depends(get_db), user: UserToJwt = Depends(get_current_user)):
     response = create_daily_rating(daily_rating, db)
     return response
