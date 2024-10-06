18301273e7caa9ce4632c6dff989655c25f9d4ad
BrayanAlq <brayan.alquizar@unmsm.edu.pe>
2024-10-05 19:16:34 -0500

    add: route daily_rating

--- Desarrollo/MindSoft/Implementación/Backend/src/controllers/daily_rating.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/controllers/daily_rating.py
@@ -8,13 +8,13 @@ from sqlalchemy.orm import Session
 
 from ..utils.jwt_util import get_current_user
 
-def get_daily_rating(db: Session = Depends(get_db), user: UserToJwt = Depends(get_current_user)):
+def get_daily_rating(db: Session , user: UserToJwt):
     daily_rating = db.query(DailyRatingResponse).filter(DailyRatingResponse.user_id == user.id).first()
     if not daily_rating:
         raise HTTPException(status_code=404, detail="Daily rating not found")
     return daily_rating
 
-def create_daily_rating(daily_rating: DailyRatingCreate, db: Session = Depends(get_db)):
+def create_daily_rating(daily_rating: DailyRatingCreate, db: Session):
     db_daily_rating = DailyRatingCreate
     db.add(db_daily_rating)
     db.commit()
--- Desarrollo/MindSoft/Implementación/Backend/src/routes/daily_rating.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/routes/daily_rating.py
@@ -2,3 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from ..schemas.daily_rating import DailyRating
+from ..services.database import get_db
+from ..utils.jwt_util import get_current_user
+from ..controllers.daily_rating import get_daily_rating, create_daily_rating
+
+daily_rating_router = APIRouter()
+
+@daily_rating_router.get("/user-daily_rating")
+def get_user_daily_rating(db: Session = Depends(get_db), user: UserToJwt = Depends(get_current_user)):
+    response = get_daily_rating(db, user)
+    return response
+
+@daily_rating_router.post("/user-daily_rating")
+def create_user_daily_rating(daily_rating: DailyRating, db: Session = Depends(get_db), user: UserToJwt = Depends(get_current_user)):
+    response = create_daily_rating(daily_rating, db)
+    return response
