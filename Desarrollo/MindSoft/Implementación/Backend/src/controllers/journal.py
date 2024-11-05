57f9946fd7b4554101db08290596f57173c6ca34
deyvigo <deyvi.gomez@unmsm.edu.pe>
2024-11-05 18:38:07 -0500

    fix: response of journal post

--- Desarrollo/MindSoft/Implementación/Backend/src/controllers/journal.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/controllers/journal.py
@@ -5,29 +5,18 @@ from collections import defaultdict
 from ..models import Journal
 from ..schemas.user import UserToJwt
 from ..schemas.journal import JournalCreateDTO
-
-spanish_months = {
-  "january": "enero",
-  "february": "febrero",
-  "march": "marzo",
-  "april": "abril",
-  "may": "mayo",
-  "june": "junio",
-  "july": "julio",
-  "august": "agosto",
-  "september": "septiembre",
-  "october": "octubre",
-  "november": "noviembre",
-  "december": "diciembre"
-}
+from ..utils.spanish_montsh import spanish_months
 
 async def create_journal(journal: JournalCreateDTO, db: Session, user: UserToJwt):
   new_journal = Journal(description=journal.description, date=journal.date.strftime("%Y-%m-%d"), user_id=user.user_id)
   db.add(new_journal)
   db.commit()
   db.refresh(new_journal)
+  month_string = spanish_months[journal.date.strftime("%B").lower()]
+  year = journal.date.strftime("%Y")
+  date_string = f"{month_string} {year}"
   db.close()
-  return { "message": "journal created successfully", "journal": new_journal }
+  return { "message": "journal created successfully", "data": { "date_string": date_string, "journal": new_journal } }
 
 async def get_journal(db: Session, user: UserToJwt):
   journals = db.query(Journal).filter(Journal.user_id == user.user_id).all()
@@ -43,7 +32,7 @@ async def get_journal(db: Session, user: UserToJwt):
 
   journals_list = [
     {
-      "date": date_string,
+      "date_string": date_string,
       "journals": journals_list
     }
     for date_string, journals_list in journal_by_months.items()
