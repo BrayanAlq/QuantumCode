9055f618df9439ebc8f8fc1f6dc7c6c305b4c047
BrayanAlq <brayan.alquizar@unmsm.edu.pe>
2024-11-05 12:40:09 -0500

    fix: replace month-stats to week-stats

--- Desarrollo/MindSoft/Implementación/Backend/src/controllers/stat.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/controllers/stat.py
@@ -1,30 +1,31 @@
 from sqlalchemy import extract, func
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
-from ..schemas.stat import MonthStats
+from ..schemas.stat import WeekStats
 from ..models import DailyRating, MoodRating, MoodRatingDetail, Mood
 from ..schemas.user import UserToJwt
 
-def get_moods_stats_by_month(db: Session, user: UserToJwt, month_stats: MonthStats):
-    month = month_stats.date.month
-    year = month_stats.date.year
-    
-    month_stats = (
+def get_moods_stats_by_week(db: Session, user: UserToJwt, week_stats: WeekStats):
+    week = week_stats.date.isocalendar().week
+    year = week_stats.date.year
+
+    week_stats = (
         db.query(Mood.mood, func.count(MoodRatingDetail.mood_rating_detail_id).label("total_mood_ratings"))
         .join(MoodRating, MoodRatingDetail.mood_rating_id == MoodRating.mood_rating_id)
         .join(Mood, MoodRatingDetail.mood_id == Mood.mood_id)
         .filter(MoodRating.user_id == user.user_id)
-        .filter(extract('month', MoodRating.date) == month)
+        .filter(extract('week', MoodRating.date) == week)
         .filter(extract('year', MoodRating.date) == year)
         .group_by(Mood.mood)
         .all()
     )
-    if not month_stats:
-        raise {"message": "No moods found"}
     
-    month_stats_dict = [{"mood": mood, "total_mood_ratings": total} for mood, total in month_stats]
+    if not week_stats:
+        return {"message": "No moods found for this week"}
+    
+    week_stats_dict = [{"mood": mood, "total_mood_ratings": total} for mood, total in week_stats]
 
-    return month_stats_dict
+    return week_stats_dict
 
 def get_moods_stats(db: Session, user: UserToJwt):
     all_stats = (
@@ -55,19 +56,21 @@ def get_rating_days(db: Session, user: UserToJwt):
 
     return {"average_rating": float(average_rating)}
 
-def get_rating_days_by_month(db: Session, user: UserToJwt, month_stats: MonthStats):
-    month = month_stats.date.month
-    year = month_stats.date.year
+def get_rating_days_by_week(db: Session, user: UserToJwt, week_stats: WeekStats):
+    week = week_stats.date.isocalendar().week
+    year = week_stats.date.year
+    
     result = (
         db.query(func.avg(DailyRating.rating).label("average_rating"))
         .filter(DailyRating.user_id == user.user_id)
-        .filter(extract('month', DailyRating.date) == month)
+        .filter(extract('week', DailyRating.date) == week)
         .filter(extract('year', DailyRating.date) == year)
         .first()
     )
+    
     average_rating = result[0] if result and result[0] is not None else None
-    
+
     if not average_rating:
-        return {"message": "No ratings found"}
-    
-    return average_rating
+        return {"message": "No ratings found for this week"}
+
+    return {"average_rating": average_rating}
--- Desarrollo/MindSoft/Implementación/Backend/src/routes/stat.py
+++ Desarrollo/MindSoft/Implementación/Backend/src/routes/stat.py
@@ -5,13 +5,13 @@ from ..schemas.stat import MonthStats
 from ..services.database import get_db
 from ..utils.jwt_util import get_current_user
 from ..schemas.user import UserToJwt
-from ..controllers.stat import get_rating_days, get_rating_days_by_month, get_moods_stats, get_moods_stats_by_month
+from ..controllers.stat import get_rating_days, get_rating_days_by_week, get_moods_stats, get_moods_stats_by_week
 
 stat_router = APIRouter()
 @stat_router.get("/stats-by-month")
-def get_stats_by_month(month_stats: MonthStats, db: Session = Depends(get_db), user: UserToJwt = Depends(get_current_user)):
-    moods = get_moods_stats_by_month(db, user, month_stats)
-    rating = get_rating_days_by_month(db, user, month_stats)
+def get_stats_by_week(month_stats: MonthStats, db: Session = Depends(get_db), user: UserToJwt = Depends(get_current_user)):
+    moods = get_moods_stats_by_week(db, user, month_stats)
+    rating = get_rating_days_by_week(db, user, month_stats)
     response = {"moods_stats": moods, "rating_stats": rating}
     return response
 
